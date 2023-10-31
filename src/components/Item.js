3d08ee3e69eab69ccad57e1cb6cc7564bf2c3d8b
root <root@DESKTOP-UEO62DE.localdomain>
2023-10-31 23:51:47 +0300

    completed code Challenge

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,31 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem}) {
+
+  function handleDeleteClick () {
+      fetch(`http://localhost:4000/items/${id}`, {
+        method : 'DELETE',       
+      })
+      .then (res => res.json())
+      .then (() => onDeleteItem(item))
+  }
+
+  function handleAddToCartClick (){
+    console.log ("clicked item" , item)
+    fetch (`http://localhost:4000/items/${id}`, {
+      method : 'PATCH',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify ({
+        isInCart : !item.isInCart,
+      }),
+    })
+.then (res => res.json())
+.then ((updatedItem) => onUpdateItem(updatedItem))
+
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
